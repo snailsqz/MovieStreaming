8a011360e48f7b20abaeccf4cadb9ade7dad9a80
snails <snailsqz@gmail.com>
2024-02-16 02:13:07 +0700

    asdads

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const path = require("path");
 var bodyParser = require("body-parser");
 
 const base_url = "http://localhost:3000";
 
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
